6801dbcf5677d2f2bef53178a61f1cf89333b5b0
Nicolas Maldonado <nlmaldonadog@unal.edu.co>
2018-04-02 21:02:13 -0500

    Queries and mutations sessions

--- src/sessions/resolvers.js
+++ src/sessions/resolvers.js
@@ -8,16 +8,41 @@ const resolvers = {
     Query: {
         userById: (_, { id }) =>
 			generalRequest(`${URL1}/${id}/`, 'GET'),
+		validateSession: (_, { headersSession }) => {
+			let headers = {
+			'access-token' : headersSession.token,
+			'client' : headersSession.client,
+			'uid' : headersSession.uid
+			}
+			console.log( headersSession )
+			console.log( headers )
+			generalRequest(`${URL}/validate_token`, 'GET', {}, headers).then((response) => {
+				console.log(response)
+				let user = response.body.data
+				user['token'] = response.headers['access-token']
+				user['type'] = response.headers['token-type']
+				user['client'] = response.headers['client']
+				console.log( user )
+				return user
+			})}
     },
 	Mutation: {
 		createSession: (_, { session }) =>
 			generalRequest(`${URL}/sign_in`, 'POST', session, true).then((response) => {
 				let user = response.body.data
 				user['token'] = response.headers['access-token']
+				user['type'] = response.headers['token-type']
+				user['client'] = response.headers['client']
 				return user
 			}),
-/*		deleteSession: (_, { id, session }) =>
-			generalRequest(`${URL}/sing_out`, 'DELETE', session)*/
+		deleteSession: (_, { headersSession }) => {
+			let headers = {
+			'access-token' : headersSession.token,
+			'client' : headersSession.client,
+			'uid' : headersSession.uid
+			}
+			generalRequest(`${URL}/sign_out`, 'DELETE', {}, headers)
+		}
 	}
 };
 
--- src/sessions/typeDefs.js
+++ src/sessions/typeDefs.js
@@ -3,7 +3,6 @@ type User{
     id: Int!
     email: String!
     provider: String!
-    uid: String!
     name: String!
     nickname: String!
     image: String
@@ -12,27 +11,40 @@ type User{
 type LoginData{
     id: Int!
     email: String!
-    uid: String!
     name: String!
     nickname: String!
     image: String
-    token: String
+    token: String!
+    type: String!
+    client: String!
 }
 
 type Data{
     data: User! 
 }
 
+type Success{
+    success: String! 
+}
+
 input SessionInput {
     email: String!
     password: String!
+}
+
+input Headers {
+    uid: String!
+    token: String!
+    client: String!
 }
 `;
 
 export const sessionsQueries = `
     userById(id: Int!): Data!
+    validateSession(headersSession: Headers!): LoginData!
 `;
 
 export const sessionsMutations = `
     createSession(session: SessionInput!): LoginData!
+    deleteSession(headersSession: Headers!): Success!
 `;
