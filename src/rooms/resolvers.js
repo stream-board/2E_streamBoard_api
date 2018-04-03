89f8e7cc7f1def346d6e22199f53a477012162e3
AFelipeGA <pipeta250498@gmail.com>
2018-04-03 15:33:46 -0500

    Chained room creation

--- src/rooms/resolvers.js
+++ src/rooms/resolvers.js
@@ -1,9 +1,13 @@
 import { generalRequest, getRequest } from '../utilities';
 import { url, port, entryPoint } from './server';
 import * as users from '../sessions/server';
+import * as chat from '../chat/server';
+import * as board from '../board/server';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
-const usersURL = `http://${users.url}:${users.port}/${users.entryPoint1}`
+const usersURL = `http://${users.url}:${users.port}/${users.entryPoint1}`;
+const chatURL = `http://${chat.url}:${chat.port}/${chat.entryPoint}`;
+const boardURL = `http://${board.url}:${board.port}/${board.entryPoint}`;
 
 const resolvers = {
 	Query: {
@@ -14,15 +18,24 @@ const resolvers = {
 	},
 	Mutation: {
 		createRoom: (_, { room }) =>
-			generalRequest(`${URL}`, 'POST', room),
-		joinRoom: (_, { room }) =>
 			generalRequest(`${URL}`, 'POST', room).then((response) => {
-				console.log(response)
+				let boardRoom = {
+					id: response.idRoom,
+					admin: response.idOwner
+				}
+				let chatRoom = {
+					id: response.idRoom
+				}
+				generalRequest(`${boardURL}/room`, 'POST', boardRoom).then((data) => data)
+				generalRequest(`${chatURL}/`, 'POST', chatRoom)
 				return response
-			}),	
+			}),
+		joinRoom: (_, { room }) =>
+			generalRequest(`${URL}`, 'POST', room),
 		deleteRoom: (_, { roomDelete }) =>
 			generalRequest(`${URL}/${roomDelete.idRoom}`, 'DELETE', roomDelete)
 	}
 };
 
 export default resolvers;
+
