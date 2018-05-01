a0ca1e2c40d6a6586d563233a66d3a3277110f20
Laura Cárdenas <lacardenasv@unal.edu.co>
2018-04-30 21:38:22 -0500

    servers url changed

--- src/board/server.js
+++ src/board/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.BOARD_URL || 'ec2-34-228-226-216.compute-1.amazonaws.com'
+export const url = process.env.BOARD_URL || 'board-ms'
 export const port = process.env.BOARD_PORT || '4002'
 export const entryPoint = process.env.BOARD_ENTRY || 'api'
--- src/rooms/server.js
+++ src/rooms/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.ROOMS_URL || 'ec2-35-168-17-78.compute-1.amazonaws.com';
+export const url = process.env.ROOMS_URL || 'rooms-ms';
 export const port = process.env.ROOMS_PORT || '4001';
 export const entryPoint = process.env.ROOMS_ENTRY || 'rooms';
