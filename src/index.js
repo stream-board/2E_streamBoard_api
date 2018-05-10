565078abd41915a571db0275255ffc58a28cb404
AFelipeGA <afguerreroa@unal.edu.co>
2018-05-10 14:23:42 -0500

    Test auth

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(koaCors());
 // read token from header
 app.use(async (ctx, next) => {
   console.log(ctx);
-  console.log(ctx.req);
+  console.log(ctx.req.body);
 	if (ctx.header.authorization) {
 		const token = ctx.header.authorization.match(/Bearer ([A-Za-z0-9]+)/);
 		if (token && token[1]) {
